9b07ed381116fa6ecaaa4262d78f143d5826440c
amelo <alexmelo77@gmail.com>
2020-08-02 10:48:47 -0300

    Moving test methods to a class

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,20 +10,20 @@ def client():
         yield client
 
 
-def test_hello_get(client):
-    response = client.get('http://localhost:5000/hello_get?myint=1')
-    print(response)
-    assert response.status_code == 200
-    d = literal_eval(response.data.decode('utf8'))
-    assert d == {'myint': 1}
+class TestApp:  # <----- We can also put the test methods inside a class
+    def test_hello_get(self, client):
+        response = client.get('http://localhost:5000/hello_get?myint=1')
+        print(response)
+        assert response.status_code == 200
+        d = literal_eval(response.data.decode('utf8'))
+        assert d == {'myint': 1}
 
-
-def test_hello_post(client):
-    payload = {'code': 7}
-    response = client.post(
-        'http://localhost:5000/hello_post', data=payload)
-    assert response.status_code == 200
-    d = literal_eval(response.data.decode('utf8'))
-    assert d == {"code squared": 49}
+    def test_hello_post(self, client):
+        payload = {'code': 7}
+        response = client.post(
+            'http://localhost:5000/hello_post', data=payload)
+        assert response.status_code == 200
+        d = literal_eval(response.data.decode('utf8'))
+        assert d == {"code squared": 49}
 
 
